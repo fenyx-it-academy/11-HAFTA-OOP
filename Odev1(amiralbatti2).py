639649ff42c1a707453fb9ba15c6beb61fb7696e
ilhami3455 <ilhami_3455@hotmail.com>
2019-08-30 09:13:40 +0200

    11.Hafta Odevler - Ilhami

---
+++ Odev1(amiralbatti2).py
@@ -0,0 +1,5 @@
+#Odev 1: Gecen hafta basladigimiz amiral batti oyununu gelistirecegiz. Bu hafta kodunuzu Object Oriented sekline
+#cevirmenizi istiyoruz. Ayrica randomsuz yapanlarin random gemi yerlestirme ozelligini eklemelerini bekliyoruz.
+#Bonus ozellik olarak oyunun bilgisayara karsi oynanan iki kisilik versiyonunu yapabilirsiniz.
+
+class amiralbatti():
